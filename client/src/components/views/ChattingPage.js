1356f2906e99936105e85dce665f66965ffe21f1
MinCher0504 <sdkyu12345@gmail.com>
2022-12-05 20:32:00 +0900

    Update chat page : Adjust screen proportions

--- client/src/components/views/ChattingPage.js
+++ client/src/components/views/ChattingPage.js
@@ -27,24 +27,24 @@ export default function ChattingPage() {
   // Input-Container : 사용자 입력창
 
   let html = (
-    <div id="MainPage-Container">
-      <script src="https://meet.jit.si/external_api.js"></script>
-      <div id="NabBar">
-        <div className="ChatRoom-info">
-          <div id="chatroom">{chatRoom}</div>
-          <div className="Member-Cnt">{memberCount}</div>
+    <div id='MainPage-Container'>
+      <script src='https://meet.jit.si/external_api.js'></script>
+      <div id='NabBar'>
+        <div className='ChatRoom-info'>
+          <div id='chatroom'>{chatRoom}</div>
+          <div className='Member-Cnt'>{memberCount}</div>
         </div>
-        <form onSubmit={LeaveToLoginPage} id="leave">
-          <button className="ChatRoom-exit">
-            <i class="fa-solid fa-house"></i>
+        <form onSubmit={LeaveToLoginPage} id='leave'>
+          <button className='ChatRoom-exit'>
+            <i class='fa-solid fa-house'></i>
           </button>
         </form>
       </div>
-      <div className="Video-Chat-Input-container">
-        <div id="Video-Interface">
+      <div className='Video-Chat-Input-container'>
+        <div id='Video-Interface'>
           <JitsiMeeting
-            className="video"
-            domain="meet.jit.si"
+            className='video'
+            domain='meet.jit.si'
             roomName={chatRoom}
             configOverwrite={{
               startWithAudioMuted: true,
@@ -65,18 +65,18 @@ export default function ChattingPage() {
             }
             getIFrameRef={(iframeRef) => {}}
           />
+          <span className='hide'></span>
         </div>
-        <div className="hide"></div>
 
-        <div className="Chat-Input-Container">
-          <div id="Chat-Container">
-            <ul id="chat"></ul>
+        <div className='Chat-Input-Container'>
+          <div id='Chat-Container'>
+            <ul id='chat'></ul>
           </div>
 
-          <div id="Input-Container">
-            <input type="text" id="User-Input" />
-            <button id="submit" onClick={SendText}>
-              <i class="fa-solid fa-arrow-up"></i>
+          <div id='Input-Container'>
+            <input type='text' id='User-Input' />
+            <button id='submit' onClick={SendText}>
+              <i class='fa-solid fa-arrow-up'></i>
             </button>
           </div>
         </div>
